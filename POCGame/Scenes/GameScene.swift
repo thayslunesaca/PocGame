690cca0dc16cebadb292f9516bab247bb393a71f
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2023-03-27 01:46:58 -0300

    add jump player

--- POCGame/Scenes/GameScene.swift
+++ POCGame/Scenes/GameScene.swift
@@ -10,9 +10,9 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    // Mark: Properties
+    // MARK: Properties
     var ground: SKSpriteNode!
- //   var player: SKSpriteNode!
+    var player: SKSpriteNode!
     var cameraNode = SKCameraNode()
     
     var cameraMovePointPerSecond: CGFloat = 450.0
@@ -43,7 +43,12 @@ class GameScene: SKScene {
 
     }
     
-    // Mark: Systems
+    var onGround = true
+    var velocityY: CGFloat = 0.0
+    var gravity: CGFloat = 0.6
+    var playerPosY: CGFloat = 0.0
+    
+    // MARK: Systems
     
     override func didMove(to view: SKView) {
         setupNodes()
@@ -56,17 +61,42 @@ class GameScene: SKScene {
             dt = 0
         }
         lastUpdateTime = currentTime
-        moveCamera()
+//        moveCamera()
+        velocityY += gravity
+        player.position.y -= velocityY
+        
+        if player.position.y < playerPosY {
+            player.position.y = playerPosY
+            velocityY = 0.0
+            onGround = true
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        if onGround {
+            onGround = false
+            velocityY = -25.0
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        if velocityY < -12.5 {
+            velocityY = -12.5
+        }
     }
 }
-// Mark: Configurations
+
+// MARK: Configurations
 
 extension GameScene {
     
     func setupNodes() {
         createBG()
         createGround()
-        setupCamera()
+        createPlayer()
+//        setupCamera()
     }
     
     func createBG() {
@@ -86,41 +116,52 @@ extension GameScene {
             ground.name = "Ground"
             ground.anchorPoint = .zero
             ground.zPosition = 1.0
-            ground.position = CGPoint(x: CGFloat(i)*ground.frame.width, y: 0.0)
+            ground.position = CGPoint(x: CGFloat(i) * ground.frame.width, y: 0.0)
             addChild(ground)
         }
     }
     
-    func setupCamera(){
-        addChild(cameraNode)
-        camera = cameraNode
-        cameraNode.position = CGPoint(x: frame.midX, y: frame.midY)
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "ninja")
+        player.name = "Ninja"
+        player.zPosition = 5.0
+        player.position = CGPoint(
+            x: frame.width/2.0 - 100.0,
+            y: player.frame.height * 1.1)
+        playerPosY = player.position.y
+        addChild(player)
     }
     
-    func moveCamera() {
-        let amountToMove = CGPoint(x: cameraMovePointPerSecond *
-                                   CGFloat(dt), y: 0.0)
-        cameraNode.position += amountToMove
-        
-        // BACKGROUND
-        enumerateChildNodes(withName: "BG") {(node, _) in
-            let node = node as! SKSpriteNode
-            
-            if node.position.x + node.frame.width < self.cameraRect.origin.x {
-                node.position = CGPoint(x: node.position.x + node.frame.width*2.0,y: node.position.y)
-            }
-            
-        }
-        //GROUND
-        enumerateChildNodes(withName: "Ground") {(node, _) in
-            let node = node as! SKSpriteNode
-            
-            if node.position.x + node.frame.width < self.cameraRect.origin.x {
-                node.position = CGPoint(x: node.position.x + node.frame.width*2.0,y: node.position.y)
-            }
-            
-        }
-        
-    }
+//    func setupCamera(){
+//        addChild(cameraNode)
+//        camera = cameraNode
+//        cameraNode.position = CGPoint(x: frame.midX, y: frame.midY)
+//    }
+    
+//    func moveCamera() {
+//        let amountToMove = CGPoint(x: cameraMovePointPerSecond *
+//                                   CGFloat(dt), y: 0.0)
+//        cameraNode.position += amountToMove
+//
+//        // BACKGROUND
+//        enumerateChildNodes(withName: "BG") {(node, _) in
+//            let node = node as! SKSpriteNode
+//
+//            if node.position.x + node.frame.width < self.cameraRect.origin.x {
+//                node.position = CGPoint(x: node.position.x + node.frame.width*2.0,y: node.position.y)
+//            }
+//
+//        }
+//        //GROUND
+//        enumerateChildNodes(withName: "Ground") {(node, _) in
+//            let node = node as! SKSpriteNode
+//
+//            if node.position.x + node.frame.width < self.cameraRect.origin.x {
+//                node.position = CGPoint(x: node.position.x + node.frame.width*2.0,y: node.position.y)
+//            }
+//
+//        }
+//
+//    }
     
 }
